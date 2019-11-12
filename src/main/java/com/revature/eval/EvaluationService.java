2fa9c24df36add5a7da3b503afd8f38af769ccd5
sekoudosso82 <sekou.dosso82@gmail.com>
2019-11-12 09:33:57 -0500

    first commit

--- src/main/java/com/revature/eval/EvaluationService.java
+++ src/main/java/com/revature/eval/EvaluationService.java
@@ -2,8 +2,9 @@ package com.revature.eval;
 
 import java.util.List;
 import java.util.Map;
-
-public class EvaluationService {
+import java.util.*;
+public class EvaluationService 
+{
 
 	/**
 	 * 1. Without using the StringBuilder or StringBuffer class, write a method that
@@ -12,8 +13,17 @@ public class EvaluationService {
 	 * @param string
 	 * @return
 	 */
-	public String reverse(String string) {
-		// TODO Write an implementation for this method declaration
+	
+	public String reverse(String string) 
+	{
+		System.out.println("original string is: " +string);
+
+		String reversString = "" ;
+		for (int i = string.length()-1 ; i >= 0; i--)
+		{
+			reversString = reversString + string.charAt(i);
+		}
+		System.out.println("the reverse of this string is: " +reversString);
 		return null;
 	}
 
@@ -26,11 +36,20 @@ public class EvaluationService {
 	 * @param phrase
 	 * @return
 	 */
-	public String acronym(String phrase) {
-		// TODO Write an implementation for this method declaration
+	
+	Scanner sc = new Scanner(System.in);
+	
+	public String acronym(String phrase) 
+	{
+		System.out.println("Enter string to reverse:");
+        String stringInput = sc.nextLine();
+        
+        String myAcronym = stringInput.replaceAll("\\B.|\\P{L}", "").toUpperCase();
+        
+        System.out.println("the acronym for this phrase is: " + myAcronym );
+      	
 		return null;
 	}
-	
 	
 	/**
 	 * 3. Given a word, compute the scrabble score for that word.
@@ -47,10 +66,35 @@ public class EvaluationService {
 	 * @param string
 	 * @return
 	 */
-	public int getScrabbleScore(String string) {
-		// TODO Write an implementation for this method declaration
-		return 0;
-	}
+	
+	public static int getScrabbleScore(String string) 
+	{
+        String stringInputToUppercase = string.toUpperCase(); // convert input to uppercase
+        // System.out.println("the converting input to uppercase is: " + stringInputToUppercase);
+        String s1 = "AEIOULNRST"; //1
+        String s2 = "DG"; //2
+        String s3 = "BCMP"; //3
+        String s4 = "FHVWY"; //4
+        String s5 = "K"; //5
+        String s6 = "J,X"; //8
+        String s7 = "QZ"; //10
+
+        int counter = 0; // initialize a counter to compute the sum of different chars
+        char[] charArray = stringInputToUppercase.toCharArray();
+
+        for (char c : charArray )
+        {
+            if (s1.indexOf(c) != -1){ counter = counter + 1 ; }
+            else if (s2.indexOf(c) != -1){ counter = counter + 2 ; }
+            else if (s3.indexOf(c) != -1){ counter = counter + 3 ; }
+            else if (s4.indexOf(c) != -1){ counter = counter + 4 ; }
+            else if (s5.indexOf(c) != -1){ counter = counter + 5; }
+            else if (s6.indexOf(c) != -1){ counter = counter + 8; }
+            else if (s7.indexOf(c) != -1){ counter = counter + 10; }
+        }
+        System.out.println("total: " + counter);
+        return 0;
+    }
 	
 	
 	/**
@@ -62,8 +106,28 @@ public class EvaluationService {
 	 * @param string
 	 * @return 
 	 */
-	public Map<String, Integer> wordCount(String string) {
-		// TODO Write an implementation for this method declaration
+	
+	public Map<String, Integer> wordCount(String string) 
+	{
+		// split the input string and add each element to a string array
+        // use for loop to iterate over the array
+        // if element is not in HashMap add it with value 1
+        // else add 1 to the value
+
+        String[] stringArray = string.split(" ");
+        Map<String, Integer> myHashMap = new HashMap<String, Integer>();
+
+        for (String s: stringArray )
+        {
+            if (myHashMap.containsKey(s))
+            { myHashMap.put(s, myHashMap.get(s) +1 ); }
+            else
+            { myHashMap.put(s, 1); }
+        }
+
+        Set<String> keysSet = myHashMap.keySet();
+        for (String s : keysSet)
+        {   System.out.println( s +":" + myHashMap.get(s)+" " ); }
 		return null;
 	}
 	
@@ -102,12 +166,36 @@ public class EvaluationService {
 	 * binary search is a dichotomic divide and conquer search algorithm.
 	 * 
 	 */
-	static class BinarySearch<T> {
+	static class BinarySearch<T>
+    {
 		private List<T> sortedList;
+		public int indexOf(T t)
+        {
 
-		public int indexOf(T t) {
-			// TODO Write an implementation for this method declaration
+			// get the list size , our list is T
+            int listSize = (sortedList.size() - 1);
+            // start the search at the 2 ends of the list
+            int lenftEnd = 0;
+            int rightEnd = 0;
+            // divide the list by 2 and iterate over each list
+            while (lenftEnd <= rightEnd)
+            {
+                // calculate the middle ot each list
+                int midPoint = (lenftEnd + rightEnd)/2;
+                System.out.println("midPoint: "+ midPoint);
+                // if search item is midPoint then return midPoint index
+//                if (sortedList.indexOf(midPoint) == t)
+                if (sortedList.indexOf(t) == midPoint)
+                    {   return midPoint; }
+                // search the item in other subList
+                else if ( sortedList.indexOf(t)>0)
+                    {   lenftEnd = midPoint + 1; }
+                else
+                    { rightEnd = midPoint -1 ; }
+            }
+            System.out.println(" found at " + sortedList.indexOf(t));
 			return 0;
+
 		}
 
 		public BinarySearch(List<T> sortedList) {
